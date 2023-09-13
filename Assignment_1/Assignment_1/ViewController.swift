e8daeb1bddd253cda7b0473c827bfa7495e6fd99
majidpawar9 <majidpawar9@gmail.com>
2023-09-13 19:01:40 -0400

    init

---
+++ Assignment_1/Assignment_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_1
+//
+//  Created by Majid Pawar on 2023-09-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
